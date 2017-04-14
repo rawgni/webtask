f94071d73c5c39db9f3cb43920c3ef7be3e2c4e4
rawgni <ingwar.wirjawan@gmail.com>
2017-04-14 13:43:58 +0700

    replace double quote with single quote

--- execute_python/execpy.js
+++ execute_python/execpy.js
@@ -1,7 +1,9 @@
 const exec = require('child_process').exec;
 
 module.exports = (ctx, done) => {
-    var body_array = ctx.body_raw.split("\r\n");
+    /* replace all double quote with single qoute */
+    var body = ctx.body_raw.replace(/\"/g,"'")
+    var body_array = body.split("\r\n");
     /* execute python code */
     exec("echo \"" + body_array.slice(3, body_array.length-2).join("\n") + "\" | python", (err , stdout , stderr) => {
         done(null, {
